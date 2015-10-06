8f94fd0647d970a0ccfd2c797f7a9442ef875be0
Jiyuan Qian <qianjy1990@gmail.com>
2015-10-06 00:34:37 -0400

    add test for median centering

--- test_tga.py
+++ test_tga.py
@@ -28,12 +28,32 @@ def test_tga():
     assert_array_almost_equal(X_r, X_r2)
 
 
+def test_tga_median():
+    # TGA on dense arrays
+    tga = TGA(n_components=2, random_state=1, centering='median')
+    X = iris.data
+    X_r = tga.fit(X).transform(X)
+    assert_equal(X_r.shape[1], 2)
+
+    tga = TGA(n_components=2, random_state=1, centering='median')
+    X_r2 = tga.fit_transform(X)
+    assert_array_almost_equal(X_r, X_r2)
+
+    tga = TGA(random_state=1, centering='median')
+    tga.fit(X)
+    X_r = tga.transform(X)
+    tga = TGA(random_state=1, centering='median')
+    X_r2 = tga.fit_transform(X)
+    assert_array_almost_equal(X_r, X_r2)
+
+
 def test_pca_error():
     X = [[0, 1], [1, 0]]
     for n_components in [-1, 3]:
         assert_raises(ValueError, TGA(n_components).fit, X)
     for trim_p in [-0.1, 0.6]:
         assert_raises(ValueError, TGA(trim_proportion=trim_p).fit, X)
+    assert_raises(ValueError, TGA(centering='invalid').fit, X)
 
 
 def test_tga_check_projection():
